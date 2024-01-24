acc19c8a963d581eb1cdd61aabca826cca195642
James <nah@nono.com>
2024-01-24 15:59:32 +0100

    Toevoeging van bundels

--- app/src/main/java/com/jesse/kledingnator/KeuzeKledingstuk.kt
+++ app/src/main/java/com/jesse/kledingnator/KeuzeKledingstuk.kt
@@ -40,20 +40,19 @@ class KeuzeKledingstuk : Fragment() {
         val view = inflater.inflate(R.layout.fragment_keuze_kledingstuk, container, false)
 
         view.findViewById<Button>(R.id.btn_keuze_broek).setOnClickListener {
-            val kledingstuk :
-            val kledingstuk = ¨Broek¨
+            val kledingstuk = "Broek"
             val antwoordKledingstuk = bundleOf("antwoord" to kledingstuk)
             Navigation.findNavController(view).navigate(R.id.action_keuzeKledingstuk_to_vraag1, antwoordKledingstuk)
         }
 
         view.findViewById<Button>(R.id.btn_keuze_sokken).setOnClickListener {
-            val kledingstuk = ¨Sokken¨
+            val kledingstuk = "Sokken"
             val antwoordKledingstuk = bundleOf("antwoord" to kledingstuk)
             Navigation.findNavController(view).navigate(R.id.action_keuzeKledingstuk_to_vraag1, antwoordKledingstuk)
         }
 
         view.findViewById<Button>(R.id.btn_keuze_vest).setOnClickListener {
-            val kledingstuk = ¨Vest¨
+            val kledingstuk = "Vest"
             val antwoordKledingstuk = bundleOf("antwoord" to kledingstuk)
             Navigation.findNavController(view).navigate(R.id.action_keuzeKledingstuk_to_vraag1, antwoordKledingstuk)
         }
